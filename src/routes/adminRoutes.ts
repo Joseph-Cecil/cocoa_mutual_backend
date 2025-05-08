8209cd17dfeeb7bb7ddd5793a5cd9caaa88b9ad7
Joseph-Cecil <josephgodwins015@gmail.com>
2025-05-08 04:58:54 +0000

    Working on getAllUsers

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import {authorizeAdmin, authenticateUser} from "../middleware/authMiddleware";
 import {getAllAnnualContributions, uploadAnnualContributions} from "../controllers/contribution"
+import { getAllUsers } from "../controllers/adminController";
 
 const router = express.Router();
 
 router.post("/upload-excel", authenticateUser, authorizeAdmin, uploadAnnualContributions);
 router.get("/get-staff-data", authenticateUser, authorizeAdmin, getAllAnnualContributions);
+router.get("/users", getAllUsers);
 
 export default router;
