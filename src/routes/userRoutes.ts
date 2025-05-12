7278f9a536958b80875fb01956a89aafd1e33861
Joseph Cecil Odoi <88081064+Joseph-Cecil@users.noreply.github.com>
2025-05-12 10:44:39 +0000

    new changes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import {authenticate, authenticateUser} from "../middleware/authMiddleware";
-import { getStaffData, getUserProfile } from "../controllers/userController";
+import { getUserProfile } from "../controllers/userController";
 
 const router = express.Router();
 
 router.get("/profile", authenticate, getUserProfile);
-router.get("/staff-data", getStaffData);
 
 export default router;
